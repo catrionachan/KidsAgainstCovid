e44a1496ba3a0be4a04884e7c3f5dbb510a68fa6
Jonah Walker <jonahlwalker@gmail.com>
2020-05-02 18:32:35 -0400

    home generation

--- simulation.js
+++ simulation.js
@@ -1,7 +1,9 @@
+//PEOPLE//
 var people = []
 
-function Person(id, age, socialDistancingObedience, underlyingCondition) {
+function Person(id, location, age, socialDistancingObedience, underlyingCondition) {
     this.id = id;
+    this.location = location;
     this.age = age;
     this.socialDistancingObedience = socialDistancingObedience;
     this.underlyingCondition = underlyingCondition;
@@ -13,3 +15,24 @@ for (var i = 0; i < 100; i++) {
 }
 
 console.log(people);
+
+//HOMES//
+var homes = []
+
+function Home(id, nPeople) {
+    this.id = id;
+    this.nPeople = nPeople;
+}
+
+for (var i = 0; i < 20; i++) {
+    homes[i] = new Home(i, -1);
+    nPeopleDecider = Math.random();
+    if (nPeopleDecider <= 0.2837) homes[i].nPeople = 1;
+    if (nPeopleDecider > 0.2837 && nPeopleDecider <= 0.6288) homes[i].nPeople = 2;
+    if (nPeopleDecider > 0.6288 && nPeopleDecider <= 0.7795) homes[i].nPeople = 3;
+    if (nPeopleDecider > 0.7795 && nPeopleDecider <= 0.9071) homes[i].nPeople = 4;
+    if (nPeopleDecider > 0.9071 && nPeopleDecider <= 0.9649) homes[i].nPeople = 5;
+    if (nPeopleDecider > 0.9649 && nPeopleDecider <= 1) homes[i].nPeople = 6;
+}
+
+console.log(homes);
