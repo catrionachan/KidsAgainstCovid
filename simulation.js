a31914f682350c34557fedb7a27a35767652f485
Jonah Walker <jonahlwalker@gmail.com>
2020-05-02 18:37:28 -0400

    population is based on space in homes

--- simulation.js
+++ simulation.js
@@ -1,3 +1,28 @@
+//HOMES//
+var homes = []
+
+function Home(id, nPeople) {
+    this.id = id;
+    this.nPeople = nPeople;
+}
+
+population = 0;
+
+for (var i = 0; i < 40; i++) {
+    homes[i] = new Home(i, -1);
+    nPeopleDecider = Math.random();
+    if (nPeopleDecider <= 0.2837) homes[i].nPeople = 1;
+    if (nPeopleDecider > 0.2837 && nPeopleDecider <= 0.6288) homes[i].nPeople = 2;
+    if (nPeopleDecider > 0.6288 && nPeopleDecider <= 0.7795) homes[i].nPeople = 3;
+    if (nPeopleDecider > 0.7795 && nPeopleDecider <= 0.9071) homes[i].nPeople = 4;
+    if (nPeopleDecider > 0.9071 && nPeopleDecider <= 0.9649) homes[i].nPeople = 5;
+    if (nPeopleDecider > 0.9649 && nPeopleDecider <= 1) homes[i].nPeople = 6;
+    population += homes[i].nPeople;
+}
+
+console.log(homes);
+console.log("pop: " + population);
+
 //PEOPLE//
 var people = []
 
@@ -9,30 +34,9 @@ function Person(id, location, age, socialDistancingObedience, underlyingConditio
     this.underlyingCondition = underlyingCondition;
 }
 
-for (var i = 0; i < 100; i++) {
-    people[i] = new Person(i, Math.floor(Math.random()*100), Math.random(), false);
+for (var i = 0; i < population; i++) {
+    people[i] = new Person(i, null, Math.floor(Math.random()*100), Math.random(), false);
     if (Math.random() <= 0.05) people[i].underlyingCondition = true;
 }
 
 console.log(people);
-
-//HOMES//
-var homes = []
-
-function Home(id, nPeople) {
-    this.id = id;
-    this.nPeople = nPeople;
-}
-
-for (var i = 0; i < 20; i++) {
-    homes[i] = new Home(i, -1);
-    nPeopleDecider = Math.random();
-    if (nPeopleDecider <= 0.2837) homes[i].nPeople = 1;
-    if (nPeopleDecider > 0.2837 && nPeopleDecider <= 0.6288) homes[i].nPeople = 2;
-    if (nPeopleDecider > 0.6288 && nPeopleDecider <= 0.7795) homes[i].nPeople = 3;
-    if (nPeopleDecider > 0.7795 && nPeopleDecider <= 0.9071) homes[i].nPeople = 4;
-    if (nPeopleDecider > 0.9071 && nPeopleDecider <= 0.9649) homes[i].nPeople = 5;
-    if (nPeopleDecider > 0.9649 && nPeopleDecider <= 1) homes[i].nPeople = 6;
-}
-
-console.log(homes);
