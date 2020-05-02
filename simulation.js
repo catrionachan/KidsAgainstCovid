a52d73b6f18dea8ca1b39a02e396c38b5e44f495
Jonah Walker <jonahlwalker@gmail.com>
2020-05-02 18:50:24 -0400

    people now live in houses

--- simulation.js
+++ simulation.js
@@ -1,23 +1,25 @@
 //HOMES//
 var homes = []
 
-function Home(id, nPeople) {
+function Home(id, maxPeople, currentPeople) {
     this.id = id;
-    this.nPeople = nPeople;
+    this.maxPeople = maxPeople;
+    this.currentPeople = currentPeople;
 }
 
 population = 0;
+nHomes = 40;
 
-for (var i = 0; i < 40; i++) {
-    homes[i] = new Home(i, -1);
-    nPeopleDecider = Math.random();
-    if (nPeopleDecider <= 0.2837) homes[i].nPeople = 1;
-    if (nPeopleDecider > 0.2837 && nPeopleDecider <= 0.6288) homes[i].nPeople = 2;
-    if (nPeopleDecider > 0.6288 && nPeopleDecider <= 0.7795) homes[i].nPeople = 3;
-    if (nPeopleDecider > 0.7795 && nPeopleDecider <= 0.9071) homes[i].nPeople = 4;
-    if (nPeopleDecider > 0.9071 && nPeopleDecider <= 0.9649) homes[i].nPeople = 5;
-    if (nPeopleDecider > 0.9649 && nPeopleDecider <= 1) homes[i].nPeople = 6;
-    population += homes[i].nPeople;
+for (var i = 0; i < nHomes; i++) {
+    homes[i] = new Home(i, -1, 0);
+    maxPeopleDecider = Math.random();
+    if (maxPeopleDecider <= 0.2837) homes[i].maxPeople = 1;
+    if (maxPeopleDecider > 0.2837 && maxPeopleDecider <= 0.6288) homes[i].maxPeople = 2;
+    if (maxPeopleDecider > 0.6288 && maxPeopleDecider <= 0.7795) homes[i].maxPeople = 3;
+    if (maxPeopleDecider > 0.7795 && maxPeopleDecider <= 0.9071) homes[i].maxPeople = 4;
+    if (maxPeopleDecider > 0.9071 && maxPeopleDecider <= 0.9649) homes[i].maxPeople = 5;
+    if (maxPeopleDecider > 0.9649 && maxPeopleDecider <= 1) homes[i].maxPeople = 6;
+    population += homes[i].maxPeople;
 }
 
 console.log(homes);
@@ -26,16 +28,25 @@ console.log("pop: " + population);
 //PEOPLE//
 var people = []
 
-function Person(id, location, age, socialDistancingObedience, underlyingCondition) {
+function Person(id, location, home, age, socialDistancingObedience, underlyingCondition) {
     this.id = id;
     this.location = location;
+    this.home = home;
     this.age = age;
     this.socialDistancingObedience = socialDistancingObedience;
     this.underlyingCondition = underlyingCondition;
 }
 
 for (var i = 0; i < population; i++) {
-    people[i] = new Person(i, null, Math.floor(Math.random()*100), Math.random(), false);
+    people[i] = new Person(i, null, null, Math.floor(Math.random()*100), Math.random(), false);
+    for (var j = 0; j < nHomes; j++) {
+        if (homes[j].currentPeople != homes[j].maxPeople) {
+            console.log("test");
+            people[i].home = homes[j];
+            homes[j].currentPeople++;
+            break;
+        }
+    }
     if (Math.random() <= 0.05) people[i].underlyingCondition = true;
 }
 
