cf35c43199a36278f00e4c89ba0193b23bc638b5
Jonah Walker <jonahlwalker@gmail.com>
2020-05-02 23:59:27 -0400

    death and recovery overhaul

--- simulation.js
+++ simulation.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 fs.writeFile('results.txt', '', function(err){
     if (err) console.log(err);
 });
-fs.writeFile('results.csv', 'day,infected,recovered,dead,shopped\n', function(err){
+fs.writeFile('results.csv', 'day,susceptible,infected,recovered,dead,shopped\n', function(err){
     if (err) console.log(err);
 });
 
@@ -58,10 +58,46 @@ function Person(id, infected, tSinceInfection, symptomatic, recovered, dead, loc
     this.age = age;
     this.socialDistancingObedience = socialDistancingObedience;
     this.underlyingCondition = underlyingCondition;
+
+    this.getDeathChance = function() {
+        var chance = 0;
+        if (age >= 80) {
+            chance += 0.01;
+            if (this.tSinceInfection >= 6) chance += 0.03;
+            if (this.tSinceInfection >= 11) chance += 0.05;
+        }
+        else if (age < 80 && age >= 70) {
+            chance += 0.0075;
+            if (this.tSinceInfection >= 6) chance += 0.025;
+            if (this.tSinceInfection >= 11) chance += 0.0425;
+        }
+        else if (age < 70 && age >= 60) {
+            chance += 0.005;
+            if (this.tSinceInfection >= 10) chance += 0.015;
+            if (this.tSinceInfection >= 20) chance += 0.025;
+        }
+        else if (age < 60 && age >= 50) {
+            chance += 0.00125;
+            if (this.tSinceInfection >= 10) chance += 0.005;
+            if (this.tSinceInfection >= 20) chance += 0.00875;
+        }
+        else chance += 0.001;
+        if (this.underlyingCondition) chance += 0.5;
+        return chance;
+    }
+
+    this.getRecoveryChance = function() {
+        var chance = 0;
+        if (this.tSinceInfection >= 21) chance += 0.35;
+        else if (this.tSinceInfection >= 18) chance += 0.25;
+        else if (this.tSinceInfection >= 14) chance += 0.15;
+        else if (this.tSinceInfection >= 10) chance += 0.1;
+        return chance;
+    }
 }
 
 for (var i = 0; i < population; i++) {
-    people[i] = new Person(i, false, 0, false, false, false, locations.HOME, false, null, Math.floor(Math.random()*100), Math.random(), false);
+    people[i] = new Person(i, false, 0, false, false, false, locations.HOME, false, null, Math.floor(Math.random()*99+1), Math.random(), false);
     for (h of homes) {
         if (h.currentPeople < h.maxPeople) {
             people[i].home = h;
@@ -69,7 +105,7 @@ for (var i = 0; i < population; i++) {
             break;
         }
     }
-    if (Math.random() <= 0.05) people[i].underlyingCondition = true;
+    if (Math.random() <= 0.03) people[i].underlyingCondition = true;
 }
 
 console.log(people);
@@ -83,7 +119,8 @@ people[0].infected = true;
 for (var i = 1; i <= nDays; i++) {
     //general
     for (p of people) {
-        if (p.tSinceInfection >= 15) p.infected = false, p.recovered = true;
+        if (Math.random() <= p.getDeathChance() && p.infected && !p.dead) p.infected = false, p.dead = true;
+        if (Math.random() <= p.getRecoveryChance() && p.infected && !p.dead) p.infected = false, p.recovered = true;
         else if (p.tSinceInfection >= 5) p.symptomatic = true;
         if (p.infected) p.tSinceInfection++;
     }
@@ -93,12 +130,12 @@ for (var i = 1; i <= nDays; i++) {
     shopped = 0;
     for (p of people) {
         if ((Math.random() <= 0.3*(1 - p.socialDistancingObedience) && !p.justShopped && !p.symptomatic) || (Math.random() <= 0.3*(1 - p.socialDistancingObedience && p.recovered))) {
-            p.location = locations.GROCERY_STORE, shopped++;
+            if (!p.dead) p.location = locations.GROCERY_STORE, shopped++;
         }
-        if (p.infected && p.location == locations.GROCERY_STORE) groceryInfectionChance += 0.5;
+        if (p.infected && p.location == locations.GROCERY_STORE && !p.dead) groceryInfectionChance += 0.5;
     }
     for (p of people) {
-        if (p.location == locations.GROCERY_STORE) if (Math.random() <= groceryInfectionChance) p.infected = true;
+        if (p.location == locations.GROCERY_STORE && !p.dead && !p.recovered) if (Math.random() <= groceryInfectionChance) p.infected = true;
     }
     for (p of people) {
         p.location = locations.HOME;
@@ -116,24 +153,27 @@ for (var i = 1; i <= nDays; i++) {
         if (p.recovered) recovered++;
         if (p.dead) dead++;
     }
+    susceptible = population - infected - recovered - dead;
     var sb = "";
         sb+="\nday " + i;
+        sb+="\nsusceptible " + susceptible;
         sb+="\ninfected " + infected;
         sb+="\nrecovered " + recovered;
         sb+="\ndead " + dead;
         sb+="\nshopped " + shopped;
     txtWrite('results.txt', sb);
     
-    sb = i + "," + infected + "," + recovered + "," + dead + "," + shopped;
+    sb = i + "," + susceptible + "," + infected + "," + recovered + "," + dead + "," + shopped;
     csvWrite('results.csv', sb);
     console.log("\nday " + i);
+    console.log("susceptible " + susceptible);
     console.log("infected " + infected);
     console.log("recovered " + recovered);
     console.log("dead " + dead);
     console.log("shopped " + shopped);
 }
 
-//console.log(people);
+console.log(people);
 
 function txtWrite(savePath, sb) {
     setTimeout(() => {
